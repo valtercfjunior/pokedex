f693e9431ea6f400990bdb09fc90264e652245af
Valter Junior <79730793+valtercfjunior@users.noreply.github.com>
2022-03-25 22:05:27 -0300

    API implementation

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
+
+
+function showPokemon(pokemonContentName, pokemonContentPicture, pokemonContentHeight,pokemonContentType){
+
+    contentPokemon = document.querySelector("main")
+
+    contentPokemon.innerHTML = `
+    <div class="contentPokemon">
+        <h1>${pokemonContentName}</h1>
+        <img src="${pokemonContentPicture}" alt="pokemon picture">
+        <h2> Peso: ${pokemonContentHeight}</h2>
+        <h2> Tipo: ${pokemonContentType}</h2>
+    </div>
+    `
+
+}
+
+
 function getPokemon(poke){
     axios.get(`https://pokeapi.co/api/v2/pokemon/${poke}`)
     .then(response => {
 
-        const content = document.getElementById('teste')
-        content.textContent = JSON.stringify(response.data.name)
-        console.log(response)
-    })
+        
+        pokemonContentName = JSON.stringify(response.data.name).replace(/"/g, "")
+        pokemonContentPicture = JSON.stringify(response.data.sprites.other["official-artwork"].front_default).replace(/"/g, "")
+        pokemonContentHeight = JSON.stringify(response.data.height)
+        pokemonContentType = JSON.stringify(response.data.types[0].type.name).replace(/"/g, "")
+        
+
+
+        showPokemon(pokemonContentName, pokemonContentPicture, pokemonContentHeight,pokemonContentType)
+
+    }).catch(error => {console.log(error)})
 
 }
+const enterSearch = document.querySelector("#inputSearch")
 
-$('#inputSearch').on('keydown', function (event) {
-    alert(event.value)
+enterSearch.addEventListener("keydown", (element) => {
+    if( element.key === "Enter") {
+        let pokemonName =  (enterSearch.value).toLowerCase()
+        getPokemon(pokemonName)
+
+    } 
+    
 })
-function showPokemon(element){
-    
-   
-    
-    
-    /* getPokemon(namePoke) */
-}
-
-/* getPokemon()  */
-
-
-
-
-
-
-
-    
-
 
