d0c99be1046be1ec4fa975d4f02af04b282f6188
Valter Junior <79730793+valtercfjunior@users.noreply.github.com>
2022-03-28 22:25:00 -0300

    start filters systems

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-function showPokemon(pokemonContentName, pokemonContentPicture, pokemonContentHeight,pokemonContentType){
+function showPokemon(pokemonContentName, pokemonContentPicture, pokemonContentHeight,pokemonContentWeight,pokemonContentType){
 
     contentPokemon = document.querySelector("main")
 
@@ -9,7 +9,9 @@ function showPokemon(pokemonContentName, pokemonContentPicture, pokemonContentHe
         <h1>${pokemonContentName}</h1>
         <img src="${pokemonContentPicture}" alt="pokemon picture">
         <h2> Peso: ${pokemonContentHeight}</h2>
+        <h2> Altura: ${pokemonContentWeight}</h2>
         <h2> Tipo: ${pokemonContentType}</h2>
+        <button><img src="./images/pokeball-pokemon-svgrepo-com.svg" alt="" class="addButton"></button>
     </div>
     `
 
@@ -24,11 +26,12 @@ function getPokemon(poke){
         pokemonContentName = JSON.stringify(response.data.name).replace(/"/g, "")
         pokemonContentPicture = JSON.stringify(response.data.sprites.other["official-artwork"].front_default).replace(/"/g, "")
         pokemonContentHeight = JSON.stringify(response.data.height)
+        pokemonContentWeight = JSON.stringify(response.data.weight)
         pokemonContentType = JSON.stringify(response.data.types[0].type.name).replace(/"/g, "")
         
 
 
-        showPokemon(pokemonContentName, pokemonContentPicture, pokemonContentHeight,pokemonContentType)
+        showPokemon(pokemonContentName, pokemonContentPicture, pokemonContentHeight,pokemonContentWeight,pokemonContentType)
 
     }).catch(error => {console.log(error)})
 
@@ -43,4 +46,56 @@ enterSearch.addEventListener("keydown", (element) => {
     } 
     
 })
+
+const listTypes = [
+    {pt: "Aço", eng: "steel"},
+    {pt: "Água", eng: "water"},
+    {pt: "Dragão", eng: "dragon"},
+    {pt: "Elétrico", eng: "electric"},
+    {pt: "Fada", eng: "fairy"},
+    {pt: "Fantasma", eng: "ghost"},
+    {pt: "Fogo", eng: "fire"},
+    {pt: "Gelo", eng: "ice"},
+    {pt: "Inseto", eng: "bug"},
+    {pt: "Lutador", eng: "fighting"},
+    {pt: "Normal", eng: "normal"},
+    {pt: "Pedra", eng: "rock"},
+    {pt: "Planta", eng: "grass"},
+    {pt: "Psíquico", eng: "psychic"},
+    {pt: "Sombrio", eng: "dark"},
+    {pt: "Terrestre", eng: "ground"},
+    {pt: "Venenoso", eng: "poison"},
+    {pt: "Voador", eng: "flying"},
+]
+
+function showSixPokemon(value){
+
+    
+    const type = listTypes.find(element => element.pt === value).eng
+
+    axios.get(`https://pokeapi.co/api/v2/type/${type}`).then(response =>{
+
+    let listPokemonsOfType = []
+
+    response.data.pokemon.forEach(element =>{
+        listPokemonsOfType.push(element.pokemon.name)
+    })
+
+    console.log(listPokemonsOfType)
+})
+
+    
+    
+    
+    }
+
+    
+
+    
+
+    
+
+
+
+
 
